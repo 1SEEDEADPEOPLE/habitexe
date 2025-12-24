47943fbd4b2c32d28eb4a54ccb6eb58fae53b7b2
1SEEDEADPEOPLE <137580606+1SEEDEADPEOPLE@users.noreply.github.com>
2025-12-24 20:27:24 +1300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "habit-exe-pwa-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.webmanifest",
+    "./sw.js",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            if (cached) return cached;
+            return fetch(event.request).then((resp) => {
+                if (event.request.method === "GET") {
+                    const copy = resp.clone();
+                    caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+                }
+                return resp;
+            });
+        })
+    );
+});
